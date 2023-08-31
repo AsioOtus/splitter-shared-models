015b77140b09815292d811f564dfb7c8ca997ddd
AsioOtus <asio.otus.verus@icloud.com>
2023-09-01 00:46:41 +0300

    Fix

--- Sources/SplitterNetworkModels/Models/Responses/AuthenticateResponseModel.swift
+++ Sources/SplitterNetworkModels/Models/Responses/AuthenticateResponseModel.swift
@@ -2,11 +2,11 @@ import Foundation
 import SplitterSharedModels
 
 public struct AuthenticateResponseModel: ResponseNetworkModel {
-	public let token: Tokens
+	public let tokens: Tokens
 	public let user: User
 
-	public init (token: String, user: User) {
-		self.token = token
+	public init (tokens: Tokens, user: User) {
+		self.tokens = tokens
 		self.user = user
 	}
 }
