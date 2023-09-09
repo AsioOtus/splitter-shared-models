4eb329865b8bb22dbbabaca2e7318f93ced19376
AsioOtus <asio.otus.verus@icloud.com>
2023-09-10 01:16:29 +0300

    Add tokens

--- Sources/SplitterNetworkModels/Models/Responses/RegistrationResponseModel.swift
+++ Sources/SplitterNetworkModels/Models/Responses/RegistrationResponseModel.swift
@@ -2,9 +2,11 @@ import Foundation
 import SplitterSharedModels
 
 public struct RegistrationResponseModel: ResponseNetworkModel {
-	public let user: User
+  public let tokenPair: TokenPair
+  public let user: User
 
-	public init (user: User) {
-		self.user = user
-	}
+  public init (tokenPair: TokenPair, user: User) {
+    self.tokenPair = tokenPair
+    self.user = user
+  }
 }
