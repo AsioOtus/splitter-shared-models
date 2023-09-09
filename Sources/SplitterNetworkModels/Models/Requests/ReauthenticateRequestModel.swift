cb92db1e88c997878a06d54d46647cc9d9304e06
AsioOtus <asio.otus.verus@icloud.com>
2023-09-09 19:14:03 +0300

    Fix

--- Sources/SplitterNetworkModels/Models/Requests/ReauthenticateRequestModel.swift
+++ Sources/SplitterNetworkModels/Models/Requests/ReauthenticateRequestModel.swift
@@ -1,9 +1,11 @@
 import Foundation
 
 public struct ReauthenticateRequestModel: RequestNetworkModel {
+  public let userId: UUID
   public let refreshToken: String
 
-  public init (refreshToken: String) {
+  public init (userId: UUID, refreshToken: String) {
+    self.userId = userId
     self.refreshToken = refreshToken
   }
 }
