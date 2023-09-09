c5ab083766c050c6d2eea79b42fa3f230f31cf77
AsioOtus <asio.otus.verus@icloud.com>
2023-09-10 01:26:37 +0300

    Fix

--- Sources/SplitterSharedModels/User/User.swift
+++ Sources/SplitterSharedModels/User/User.swift
@@ -3,10 +3,19 @@ import Foundation
 public struct User: Hashable, Codable {
   public let id: UUID
   public let name: String
+  public let email: String
+  public let login: String
   
-  public init (id: UUID, name: String) {
+  public init (
+    id: UUID,
+    name: String,
+    email: String? = nil,
+    login: String
+  ) {
     self.id = id
     self.name = name
+    self.email = email
+    self.login = login
   }
 
   public var firstLetters: String { .init(name.prefix(2)) }
