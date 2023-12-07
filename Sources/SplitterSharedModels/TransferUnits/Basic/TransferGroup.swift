71afb4256d044b754ca1bb7eaf68a5935ef345c2
Asio Otus <asio.otus.verus@icloud.com>
2023-12-07 23:25:59 +0200

    Update

--- Sources/SplitterSharedModels/TransferUnits/Basic/TransferGroup.swift
+++ Sources/SplitterSharedModels/TransferUnits/Basic/TransferGroup.swift
@@ -1,7 +1,6 @@
 import Foundation
 import MultitoolTree
 
-@dynamicMemberLookup
 public struct TransferGroup: Identifiable, Hashable, Codable {
   public let id: UUID
   public let info: TransferUnit.Info
@@ -46,15 +45,62 @@ public extension TransferGroup {
 
 public extension TransferGroup {
 	var isSplit: Bool {
-		transferUnits.isSingleCreditor &&
-		transferUnits.isSingleCurrency &&
-		!transferUnits.hasDuplicatedBorrowers &&
-		!transferUnits.hasNestedGroups
+		isSingleCreditor && isSingleCurrency && !hasDuplicatedBorrowers && !hasNestedGroups
 	}
 }
 
-extension TransferGroup {
-	subscript <T> (dynamicMember keyPath: KeyPath<[TransferUnit], T>) -> T {
-		transferUnits[keyPath: keyPath]
+public extension TransferGroup {
+	var amounts: [Amount] {
+		transferUnits.flatMap { $0.amounts }.compactMap { $0 }
+	}
+
+	var amountsSum: [Amount] {
+		var dictionary = [Currency: Double]()
+		amounts
+			.compactMap { $0 }
+			.forEach { dictionary[$0.currency, default: 0] += $0.value }
+
+		let amountsSum = dictionary.map { currency, value in Amount(value: value, currency: currency) }
+		return amountsSum
+	}
+
+	var singleCurrency: Currency? {
+		amountsSum.count == 1 ? amountsSum.first?.currency : nil
+	}
+
+	var isSingleCurrency: Bool {
+		singleCurrency != nil
+	}
+
+	var creditors: [User.Compact] {
+		transferUnits.flatMap { $0.creditors }.compactMap { $0 }
+	}
+
+	var uniqueCreditors: Set<User.Compact> {
+		.init(creditors)
+	}
+
+	var singleCreditor: User.Compact? {
+		uniqueCreditors.count == 1 ? uniqueCreditors.first : nil
+	}
+
+	var isSingleCreditor: Bool {
+		singleCreditor != nil
+	}
+
+	var borrowers: [User.Compact] {
+		transferUnits.flatMap { $0.borrowers }.compactMap { $0 }
+	}
+
+	var uniqueBorrowers: Set<User.Compact> {
+		.init(borrowers)
+	}
+
+	var hasDuplicatedBorrowers: Bool {
+		uniqueBorrowers.count != borrowers.count
+	}
+
+	var hasNestedGroups: Bool {
+		transferUnits.contains { $0.nodeValue != nil }
 	}
 }
--- Sources/SplitterSharedModels/TransferUnits/Basic/TransferUnit.swift
+++ Sources/SplitterSharedModels/TransferUnits/Basic/TransferUnit.swift
@@ -14,28 +14,28 @@ public extension TransferUnit {
   var amounts: [Amount?] {
     switch self {
     case .leaf(let transfer): return [transfer.amount]
-		case .node(let transferUnitGroup): return transferUnitGroup.transferUnits.amounts
+		case .node(let transferUnitGroup): return transferUnitGroup.amounts
     }
   }
 
   var summarizedAmounts: [Amount] {
     switch self {
     case .leaf(let transfer): return [transfer.amount].compactMap { $0 }
-		case .node(let transferUnitGroup): return transferUnitGroup.transferUnits.amountsSum
+		case .node(let transferUnitGroup): return transferUnitGroup.amountsSum
     }
   }
 
   var creditors: [User.Compact?] {
     switch self {
     case .leaf(let transfer): return [transfer.creditor]
-		case .node(let transferUnitGroup): return transferUnitGroup.transferUnits.creditors
+		case .node(let transferUnitGroup): return transferUnitGroup.creditors
     }
   }
 
   var borrowers: [User.Compact?] {
     switch self {
     case .leaf(let transfer): return [transfer.borrower]
-		case .node(let transferUnitGroup): return transferUnitGroup.transferUnits.borrowers
+		case .node(let transferUnitGroup): return transferUnitGroup.borrowers
     }
   }
 }
@@ -54,60 +54,4 @@ public extension TransferUnit {
     case .node(let transferGroup): return Update.node(transferGroup.update)
     }
   }
-}
-
-public extension [TransferUnit] {
-	var amounts: [Amount] {
-		flatMap { $0.amounts }.compactMap { $0 }
-	}
-
-	var amountsSum: [Amount] {
-		var dictionary = [Currency: Double]()
-		amounts
-			.compactMap { $0 }
-			.forEach { dictionary[$0.currency, default: 0] += $0.value }
-
-		let amountsSum = dictionary.map { currency, value in Amount(value: value, currency: currency) }
-		return amountsSum
-	}
-
-	var singleCurrency: Currency? {
-		amountsSum.count == 1 ? amountsSum.first?.currency : nil
-	}
-
-	var isSingleCurrency: Bool {
-		singleCurrency != nil
-	}
-
-	var creditors: [User.Compact] {
-		flatMap { $0.creditors }.compactMap { $0 }
-	}
-
-	var uniqueCreditors: Set<User.Compact> {
-		.init(creditors)
-	}
-
-	var singleCreditor: User.Compact? {
-		uniqueCreditors.count == 1 ? uniqueCreditors.first : nil
-	}
-
-	var isSingleCreditor: Bool {
-		singleCreditor != nil
-	}
-
-	var borrowers: [User.Compact] {
-		flatMap { $0.borrowers }.compactMap { $0 }
-	}
-
-	var uniqueBorrowers: Set<User.Compact> {
-		.init(borrowers)
-	}
-
-	var hasDuplicatedBorrowers: Bool {
-		uniqueBorrowers.count != borrowers.count
-	}
-
-	var hasNestedGroups: Bool {
-		contains { $0.nodeValue != nil }
-	}
 }
