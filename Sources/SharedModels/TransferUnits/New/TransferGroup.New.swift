7306a4d0ca7d2474e6b161591f05e47abdab2f9a
Asio Otus <asio.otus.verus@icloud.com>
2024-02-06 00:42:05 +0200

    SP-83 | Remove mode from new model

--- Sources/SharedModels/TransferUnits/New/TransferGroup.New.swift
+++ Sources/SharedModels/TransferUnits/New/TransferGroup.New.swift
@@ -1,20 +1,11 @@
 public extension TransferGroup {
   struct New: Hashable, Codable {
     public let info: TransferUnit.Info
-		public let mode: TransferGroup.Mode
 
     public init (
-      info: TransferUnit.Info,
-			mode: TransferGroup.Mode
+      info: TransferUnit.Info
 		) {
       self.info = info
-			self.mode = mode
     }
   }
-}
-
-public extension TransferGroup.New {
-	var isSplit: Bool {
-		mode == .split
-	}
 }
