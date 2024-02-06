d708cbe65513bb9f209ed595d405a398bf7c5b31
Asio Otus <asio.otus.verus@icloud.com>
2024-02-06 18:27:40 +0200

    SP-83 | Update models

--- Sources/NetworkModels/Models/Responses/SharedResponseModels/TransferGroup.swift
+++ Sources/NetworkModels/Models/Responses/SharedResponseModels/TransferGroup.swift
@@ -3,11 +3,11 @@ import SharedModels
 extension SharedResponseModels {
 	public struct TransferGroup: ResponseNetworkModel, Equatable {
 		public let transferGroup: SharedModels.TransferGroup
-		public let transferUnits: SharedModels.TransferUnit
+		public let transferUnits: [SharedModels.TransferUnit]
 
 		public init (
 			transferGroup: SharedModels.TransferGroup,
-			transferUnits: SharedModels.TransferUnit
+			transferUnits: [SharedModels.TransferUnit]
 		) {
 			self.transferGroup = transferGroup
 			self.transferUnits = transferUnits
