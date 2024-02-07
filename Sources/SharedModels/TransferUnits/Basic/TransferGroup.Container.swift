4a7d2bca80d2e438f8f1606ed598971f706afd2f
Asio Otus <asio.otus.verus@icloud.com>
2024-02-07 22:41:06 +0200

    SP-83 | Update models

--- Sources/SharedModels/TransferUnits/Basic/TransferGroup.Container.swift
+++ Sources/SharedModels/TransferUnits/Basic/TransferGroup.Container.swift
@@ -1,3 +1,12 @@
 extension TransferGroup {
 	public typealias Container = TransferGroupContainer<TransferGroup, TransferUnit>
 }
+
+public extension TransferGroup.Container {
+	var transferUnit: TransferUnit {
+		.init(
+			transferGroup: transferGroup,
+			transferUnits: transferUnits
+		)
+	}
+}
